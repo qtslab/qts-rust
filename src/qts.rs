c16c0de5dc500eb58386e5bed293cc2652833f3c
Young <young20050727@gmail.com>
2024-10-18 20:10:32 +0800

    refactor: use index to storge solution in QTS

--- src/qts.rs
+++ src/qts.rs
@@ -16,8 +16,8 @@ pub fn qts(
     let mut best_fit: Solution = vec![false; items.len()];
     adjust_solution(&items, &mut best_fit, capacity);
     let mut neighbors: Vec<Solution> = vec![vec![false; items.len()]; n_neighbors as usize];
-    let mut best_solution: Solution = vec![false; items.len()];
-    let mut worst_solution: Solution = vec![false; items.len()];
+    let mut best_solution_index: usize = 0;
+    let mut worst_solution_index: usize = 0;
     let mut record = Record::new(format!("csv/qts/{}.csv", test_count));
     for i in 0..max_gen {
         // println!("Generation {}/{}", i+1, max_gen);
@@ -25,23 +25,25 @@ pub fn qts(
             neighbors[j as usize] = measure(&qubits);
             adjust_solution(items, &mut neighbors[j as usize], capacity);
             if j == 0 {
-                best_solution = neighbors[j as usize].clone();
-                worst_solution = neighbors[j as usize].clone();
+                best_solution_index = j as usize;
+                worst_solution_index = j as usize;
             }
 
             if calculate_values(items, &neighbors[j as usize])
-                > calculate_values(items, &best_solution)
+                > calculate_values(items, &neighbors[best_solution_index as usize])
             {
-                best_solution = neighbors[j as usize].clone();
+                best_solution_index = j as usize;
             } else if calculate_values(items, &neighbors[j as usize])
-                < calculate_values(items, &worst_solution)
+                < calculate_values(items, &neighbors[worst_solution_index as usize])
             {
-                worst_solution = neighbors[j as usize].clone();
+                worst_solution_index = j as usize;
             }
         }
 
-        if calculate_values(items, &best_solution) > calculate_values(items, &best_fit) {
-            best_fit = best_solution.clone();
+        if calculate_values(items, &neighbors[best_solution_index])
+            > calculate_values(items, &best_fit)
+        {
+            best_fit = neighbors[best_solution_index].clone();
         }
 
         record.add_iteration(
@@ -52,7 +54,11 @@ pub fn qts(
             qubits.clone(),
         );
 
-        update_qubits(best_solution.clone(), worst_solution.clone(), &mut qubits);
+        update_qubits(
+            neighbors[best_solution_index].clone(),
+            neighbors[worst_solution_index].clone(),
+            &mut qubits,
+        );
     }
 
     let _ = record.write_file();
